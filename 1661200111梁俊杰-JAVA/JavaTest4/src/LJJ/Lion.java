0abae6b796e7100567b48369785916d62325539e
ljj2682058 <48610931+ljj2682058@users.noreply.github.com>
2019-04-09 20:16:03 +0800

    Add files via upload

---
+++ 1661200111梁俊杰-JAVA/JavaTest4/src/LJJ/Lion.java
@@ -0,0 +1,16 @@
+package LJJ;
+
+public class Lion extends Animal
+{
+	Lion(String name)
+	{
+		x=(int)(Math.random()*10);
+		y=(int)(Math.random()*10);
+		this.name=name;
+		z=3;
+	}
+	/*Lion()
+	{
+		
+	}*/
+}
---
+++ 1661200111梁俊杰-JAVA/JavaTest4/src/LJJ/Rabbit.java
@@ -0,0 +1,16 @@
+package LJJ;
+
+public class Rabbit extends Animal//อรืำ
+{
+	Rabbit(String name)
+	{
+		x=(int)(Math.random()*10);
+		y=(int)(Math.random()*10);
+		this.name=name;
+		z=1;
+	}
+	/*public Rabbit() 
+	{
+		// TODO Auto-generated constructor stub
+	}*/
+}
