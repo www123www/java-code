900a9a80f57bc4200c8c130b4cc50db2730247d2
WXRzero <48623804+WXRzero@users.noreply.github.com>
2019-05-05 21:57:25 +0800

    Add files via upload

---
+++ 标识符 用正则表达式表示/1661200101王欣蓉.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class biaoshifu {
+
+	public static void main(String[] args) {
+		String regex = "[a-zA-Z]+[A-Az-z|0-9]*\\w{3,14}";
+		Scanner scanner = new Scanner(System.in); 
+		String str = scanner.nextLine();
+		if(str.matches(regex)){
+			System.out.println(str+"此标识符合法");
+		}
+		else{
+			System.out.println(str+"此标识符不合法");
+		}
+	}
+
+}
